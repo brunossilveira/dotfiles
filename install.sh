77fe61dbe40e07074bf82b14517d481f2909564c
Bruno Saibro Silveira <djbrunosilveira@gmail.com>
2015-02-25 17:00:03 -0300

    Adding alias file and improving install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
-files=( .vimrc .zsh .gitconfig .gitignore_global .gemrc)
+#!/bin/bash
+
+files=( .vimrc .zshrc .gitconfig .gitignore_global .gemrc .alias )
 
 for filename in ${files[@]}
 do
-  [[ -s $HOME/.$filename ]] && rm $HOME/.$filename
-  ln -s $PWD/.$filename ~/.$filename
+  [[ -s $HOME/$filename ]] && rm $HOME/$filename
+  ln -s $PWD/$filename ~/$filename
+done
+
+dirs=( .vim )
+
+for dir in ${dirs[@]}
+do
+  [[ -s $HOME/$dir ]] && rm -rf $HOME/$dir
+  ln -s $PWD/$dir ~/$dir
 done
 
 source ~/.zshrc
